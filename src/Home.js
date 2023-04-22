46d1df05f6a938c2d3984dd1611002b97fcc545e
nevinod <admin@Admins-MacBook-Air.local>
2023-04-22 01:06:13 -0700

    timeout and load images

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,86 @@
-import { useState } from "react";
-import { Input } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { images } from "./images/index";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Button } from "@chakra-ui/react";
+import { Field, Form, Formik } from "formik";
 import "./Home.css";
+
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Progress,
+} from "@chakra-ui/react";
 
 function Home() {
   const [input, setInput] = useState("");
+  const [loading, setLoading] = useState(0);
+  const [submitted, setSubmitted] = useState(false);
 
-  console.log(input);
+  console.log(loading);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (submitted && loading < 100) {
+        setLoading((loading) => loading + 20);
+      }
+    }, 200);
+  }, [loading, submitted]);
+
+  console.log(loading);
+
   return (
     <ChakraProvider>
       <div className="container">
         <div className="input-container">
-          <Input
-            placeholder="Enter a description here..."
-            size="lg"
-            onChange={(e) => setInput(e.target.value)}
-          />
+          <Formik
+            initialValues={{ name: "" }}
+            onSubmit={() => {
+              console.log(input);
+              setSubmitted(true);
+              setLoading(0);
+            }}
+          >
+            {() => (
+              <Form style={{ width: "100%" }}>
+                <Field name="name">
+                  {({ field, form }) => (
+                    <FormControl>
+                      <FormLabel>Enter a description here...</FormLabel>
+                      <Input
+                        {...field}
+                        placeholder="name"
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                      />
+                      <FormHelperText>Any text you want</FormHelperText>
+                    </FormControl>
+                  )}
+                </Field>
+              </Form>
+            )}
+          </Formik>
         </div>
-        <div className="home">
-          {images.map((image, idx) => {
-            return (
-              <div className="image-container" key={`${idx}`}>
-                <img className="image" src={image} />
-              </div>
-            );
-          })}
-        </div>
+        <Progress value={80} />
+        {submitted && <h2>{`Results for ${input}`}</h2>}
+        {loading > 0 && loading < 100 && <Progress value={loading} />}
+        {loading >= 100 && (
+          <div className="home">
+            {images.map((image, idx) => {
+              return (
+                <div className="image-container" key={`${idx}`}>
+                  <img className="image" src={image} />
+                  <div className="middle">
+                    <div style={{ display: "none" }} className="text">
+                      Share
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     </ChakraProvider>
   );
